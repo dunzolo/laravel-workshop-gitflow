9e66a614f71f6334d4a56e8ac4d5512869dfea80
Reagonfly <reagon@hotmail.it>
2023-02-27 20:29:16 +0100

    adjustment of margins

--- resources/views/movies/create.blade.php
+++ resources/views/movies/create.blade.php
@@ -11,37 +11,37 @@
             <div class="col-12">
                 <form class="" action="{{ route('movie.store') }}" method="POST">
                     @csrf
-                    <div class="form-group">
+                    <div class="form-group my-3">
                         <label class="control-label" for="title">
                             Title
                         </label>
                         <input type="text" class="form-control" id="title" name="title" placeholder="Write the Title of the movie">
                     </div>
-                    <div class="form-group">
+                    <div class="form-group my-3">
                         <label class="control-label" for="nationality">
                             Nationality
                         </label>
                         <input type="text" class="form-control" id="nationality" name="nationality" placeholder="Insert movie nationality">
                     </div>
-                    <div class="form-group">
+                    <div class="form-group my-3">
                         <label class="control-label" for="release_date">
                             Release Date
                         </label>
                         <input type="date" class="form-control" id="release_date" name="release_date" placeholder="Please sett upp the Release date of the movie">
                     </div>
-                    <div class="form-group">
+                    <div class="form-group my-3">
                         <label class="control-label" for="cast">
                             Cast
                         </label>
                         <input type="text" max-lenght="100" class="form-control" id="cast" name="cast" placeholder="Please write the cast of the movie">
                     </div>
-                    <div class="form-group">
+                    <div class="form-group my-3">
                         <label class="control-label" for="cover_path">
                             Cover
                         </label>
-                        <input type="text" class="form-control" id="cover_path" name="cover_path" placeholder="Please drag the Cover">
+                        <input type="text" class="form-control" id="cover_path" name="cover_path" placeholder="Please copy the Cover link">
                     </div>
-                    <div class="form-group">
+                    <div class="form-group my-3">
                         <button type="submit" class="btn btn-lg btn-danger">Save</button>
                     </div>
                 </form>
