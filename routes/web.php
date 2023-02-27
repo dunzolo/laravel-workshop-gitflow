914c477e47a12abcd78f835bc51d564f826c3e0c
dunzolo <rossidavide108@gmail.com>
2023-02-27 15:46:03 +0100

    added controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController as PagesController;
+use App\Http\Controllers\MoviesController as MoviesController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PagesController::class,'index'])->name('homepage');
+
+Route::get('/movies', MoviesController::class, 'index');
