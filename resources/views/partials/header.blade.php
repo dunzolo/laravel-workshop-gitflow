597d67097aec1f9cb3e8d37611678520f2e834f1
Mark-dev30 <fronterremarco@gmail.com>
2023-02-27 16:17:16 +0100

    closes #1

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,18 @@
+<header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="col-12">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 w-100 justify-content-center">
+                    <li class="nav-item">
+                      <a class="nav-link" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Movies</a>
+                    </li>
+                  </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+</header>
