17f65fda4a092c93f5f48b131ef31062ff9cc2ec
Reagonfly <reagon@hotmail.it>
2023-03-02 15:51:09 +0100

    changes on web

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
 
 Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->namespace('Admin')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-    Route::get('/', [MoviesController::class, 'index'])->name('movies');
+    Route::resource('movies', MoviesController::class);
 });
 
 Route::middleware('auth')->group(function () {
